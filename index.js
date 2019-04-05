384a9b7246420c97ff003e8c8cbe887e6ffab6b4
Champy13 <hgftdrd@hotmail.com>
2019-04-05 02:18:41 -0600

    Se crea función para validar los links con validate

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 #!/usr/bin/env node
 
-const mdLinks = require('./cli.js')
+const mdLinks = require('./mdLinks.js')
 const fetch = require('node-fetch');
-const colors = require('colors');
 const chalk = require('chalk');
-const fileUser = process.argv[2];
+const pathDoc = process.argv[2];
 let options = {
   validate: false,
   stats: false
 }
 
-
-
-mdLinks(fileUser, options).then((response) => {
-
+mdLinks(pathDoc, options).then((response) => {
   if (process.argv[3] === '--validate') {
     options.validate = true;
     response.forEach((element) => {
       fetch(element.href).then(res => {
 
-        if (res.status === 200) {
-          console.log(`File: ${element.file} Titulo: ${chalk.yellowBright(element.text)} Link: ${chalk.underline.blueBright(res.url)} Status: ${chalk.greenBright(res.status)} ${res.statusText.green}(✔✔ )`);
-
-        } else if (res.status === 404) {
-          console.log(`File: ${element.file} Texto: ${chalk.yellowBright(element.text)} Link: ${element.href.red.underline} Status: ${chalk.redBright(res.status)} ${res.statusText.red} (✘ )`);
+        if (res.status >= 200 && res.status <= 309) {
+          console.log(`|Status:(✔✔ ) ${chalk.greenBright(res.status)} ${chalk.green(res.statusText)}| File: ${chalk.cyanBright(element.file)} | Text: ${chalk.yellowBright(element.text)} | Link: ${chalk.underline.blueBright(res.url)} `);
+        } else if (res.status >= 400) {
+          console.log(`|Status:(✘ ) ${chalk.redBright(res.status)} ${chalk.red(res.statusText)}| File: ${chalk.cyanBright(element.file)} | Text: ${chalk.yellowBright(element.text)} | Link: ${chalk.underline.redBright(res.url)} `);
         }
       }).catch(err => {
-        console.log(`File: ${element.file.red} Texto: ${element.text.green} Link: ${element.href.error} <--- Enlace con Problemas` + err);
+        console.log(`|Status:( ⚠ ) |File: ${chalk.cyanBright(element.file)} | Text: ${chalk.yellowBright(element.text)} | Link: ${chalk.underline.yellowBright(res.url)}<--- Este enlace presenta problemas` + err);
       });
 
     });
@@ -37,15 +32,11 @@ mdLinks(fileUser, options).then((response) => {
     //     urlArray.push(element.href)
     //     console.log(urlArray)
     //   })
-    //   console.log('El total de links en el archivo ' + fileUser + ' es: ' + response.length);
+    //   console.log('El total de links en el archivo ' + pathDoc + ' es: ' + response.length);
   } else {
-    response.forEach((element) => {
-      let file = element.file,
-        href = element.href,
-        text = element.text;
-      console.log(file + ' ' + text + ' ' + href);
+    response.forEach((element) => {        
+      console.log(`|File: ${chalk.cyanBright(element.file)} | Text: ${chalk.yellowBright(element.text)} | Link: ${chalk.underline.blueBright(element.href)}`);
     });
   }
 });
-
 module.exports = mdLinks;
--- mdLinks.js
+++ mdLinks.js
@@ -1,34 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 
-const mdLink = (pathDocument) => new Promise((resolve, err) => {
+const mdLinks = (pathDocument) => new Promise((resolve, err) => {
 
-  let toAbsolutePath = path.resolve(pathDocument);
-  let docExtension = path.extname(toAbsolutePath);
+  const toAbsolutePath = path.resolve(pathDocument);
+  const docExtension = path.extname(toAbsolutePath);
+  const linksRegExp = /\[((.+?))\]\((http|https|ftp|ftps).+?\)/g;
+  const hrefRegExp = /\((http|https|ftp|ftps).+?\)/g;
+  const textLinksRegExp = /\[.+?\]/g;
 
   if (docExtension === '.md') {
-    const linksRegExp = /\[((.+?))\]\((http|https|ftp|ftps).+?\)/g;
-    const hrefRegExp = /\((http|https|ftp|ftps).+?\)/g;
-    const textLinksRegExp = /\[.+?\]/g;
+
     const docMdToString = fs.readFileSync(toAbsolutePath).toString();
     const linksMatchInMd = docMdToString.match(linksRegExp);
     const arrayLinksMd = [];
 
     for (let i in linksMatchInMd) {
+
       let textLinksMd = linksMatchInMd[i].match(textLinksRegExp)[0];
       let urlsLinksMd = linksMatchInMd[i].match(hrefRegExp)[0];
+      
       arrayLinksMd.push({
-        text: textLinksMd.substring(1, textLinksMd.length - 1),
         href: urlsLinksMd.substring(1, urlsLinksMd.length - 1),
+        text: textLinksMd.substring(1, textLinksMd.length - 1),
         file: pathDocument
       });
     }
     resolve(arrayLinksMd);
-    //  console.log(arrayLinksMd)
   } else {
-    console.log('Archivo ingresado no es de extención .md');
+    console.log('La Extensión del archivo ingresado no es: ".md" Favor de Verificarlo!');
   }
 });
-
-
-module.exports = mdLink;
+module.exports = mdLinks;
